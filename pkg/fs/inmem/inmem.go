021ae190d774e8e662157851173dfba8ce90e81c
Solomon Okwa <solozyokwa@gmail.com>
2021-02-04 01:10:22 +0100

    refactor imem package

--- pkg/fs/inmem/inmem.go
+++ pkg/fs/inmem/inmem.go
@@ -6,15 +6,10 @@ import (
 	"github.com/dikaeinstein/godl/pkg/fs"
 )
 
-// File is an in-memory File and doesn't persist it's content to disk.
+// File represents an in-memory File and doesn't persist it's content to disk.
 // It is good for testing purposes.
 type File struct {
 	*bytes.Buffer
-}
-
-func (f *File) Write(p []byte) (int, error) {
-	f.Write(p)
-	return f.Len(), nil
 }
 
 func (*File) Close() error { return nil }
@@ -23,20 +18,20 @@ func (*File) Name() string { return "in-mem" }
 
 // FS is an in-memory based filesystem.
 type FS struct {
-	storage *bytes.Buffer
+	*bytes.Buffer
 }
 
 // NewFS returns a pointer to a new in-memory FS
 func NewFS(storage *bytes.Buffer) *FS {
-	return &FS{storage: storage}
+	return &FS{storage}
 }
 
-func (*FS) Open(name string) (fs.File, error) {
-	return &File{new(bytes.Buffer)}, nil
+func (inmem *FS) Open(name string) (fs.File, error) {
+	return &File{inmem.Buffer}, nil
 }
 
 func (inmem *FS) Create(name string) (fs.File, error) {
-	w := &File{inmem.storage}
+	w := &File{inmem.Buffer}
 	return w, nil
 }
 
@@ -46,7 +41,7 @@ func (*FS) Rename(oldPath, newPath string) error {
 
 // Content returns the stored data of the in-memory FS as a buffer of bytes.
 func (inmem *FS) Content() *bytes.Buffer {
-	return inmem.storage
+	return inmem.Buffer
 }
 
 func (*FS) RemoveAll(path string) error { return nil }
